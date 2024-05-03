9aeec803fdc5d39d58b3ff1857ef6d1a9c45d2fc
Enzo Hessel <enzo.carmo@salg.com.br>
2024-05-03 11:12:50 -0300

    feat: add initial search component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
 import Header from "./_components/header";
+import Search from "./_components/search";
 
 const Home = () => {
   return (
-    <Header />
+    <>
+      <Header />
+      <div className="px-5 pt-6">
+        <Search />
+      </div>
+    </>
   )
 }
 
