8563067ccef1018c8d5f5643f091a6f16fea8212
Enzo Hessel <enzo.carmo@salg.com.br>
2024-05-03 14:45:31 -0300

    feat: add promo banner to homo page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import CategoryList from "./_components/category-list";
 import Header from "./_components/header";
 import Search from "./_components/search";
@@ -12,6 +13,18 @@ const Home = () => {
 
       <div className="px-5 pt-6">
         <CategoryList />
+      </div>
+
+      <div className="px-5 pt-6">
+        <Image
+          src="/promo-banner01.svg"
+          alt="Até 30% de desconto em pizzas"
+          width={0}
+          height={0}
+          className="w-full h-auto object-contain"
+          sizes="100vw"
+          quality={100}
+        />
       </div>
     </>
   )
