0c1a4b2da22cc0dae13a1490e1bed3af50dcd202
Enzo Hessel <enzo.carmo@salg.com.br>
2024-07-18 13:23:02 -0300

    chore: remove warnning lint

--- app/_components/promo-banner.tsx
+++ app/_components/promo-banner.tsx
@@ -2,6 +2,7 @@ import Image, { ImageProps } from "next/image";
 
 const PromoBanner = (props: ImageProps) => {
   return (
+    // eslint-disable-next-line jsx-a11y/alt-text
     <Image
       height={0}
       width={0}
