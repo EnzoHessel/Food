d6c9564de8d889c1f6bf3c7d586d10c53d619eac
Enzo Hessel <enzo.carmo@salg.com.br>
2024-07-18 11:46:06 -0300

    fix: restaurants search and page

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -1,46 +1,25 @@
-"use client"
+import { Suspense } from "react";
+import Restaurants from "./_components/restaurants";
+import { getServerSession } from "next-auth";
+import { authOptions } from "../_lib/auth";
+import { db } from "../_lib/prisma";
 
-import { Restaurant } from '@prisma/client';
-import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
-import { searchForRestaurant } from './_actions/search';
-import Header from '../_components/header';
-import RestaurantItem from '../_components/restaurant-item';
-
-const Restaurants = () => {
-  const searchParams = useSearchParams();
-  const [restaurants, setRestaurants] = useState<Restaurant[]>([]);
-
-  useEffect(() => {
-    const fetchRestaurants = async () => {
-      const searchFor = searchParams.get("search");
-      if (!searchFor) return
-      const foundRestaurants = await searchForRestaurant(searchFor);
-        setRestaurants(foundRestaurants)
-    };
-
-    fetchRestaurants()
-  }, [searchParams]);
-
-  // TODO fazer uma tratativa para quando n tiver nada encontrado
+const RestaurantsPage = async () => {
+  const session = await getServerSession(authOptions);
+  const userFavoriteRestaurants = await db.userFavoriteRestaurant.findMany({
+    where: {
+      userId: session?.user.id,
+    },
+    include: {
+      restaurant: true,
+    },
+  });
 
   return (
-    <>
-      <Header />
-      <div className="px-5 py-6">
-        <h2 className="font-semibold text-lg mb-6">Restaurantes encotrados</h2>
-        <div className="flex flex-col gap-6 w-full">
-        {restaurants.map(restaurant => (
-          <RestaurantItem
-            key={restaurant.id}
-            restaurant={restaurant}
-            className="min-w-full max-w-full"
-          />
-        ))}
-        </div>
-      </div>
-    </>
-  )
-}
+    <Suspense>
+      <Restaurants userFavoriteRestaurants={userFavoriteRestaurants} />
+    </Suspense>
+  );
+};
 
-export default Restaurants
+export default RestaurantsPage;
